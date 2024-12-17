c3a960352f5ecea512a3efdbb8135969e00cdf09
Ryo SHIROMA <e245740@ie.u-ryukyu.ac.jp>
2024-12-17 18:21:05 +0900

    step1

---
+++ src/Report5.java
@@ -0,0 +1,6 @@
+public class Report5 {
+    public static void main(String[] args) {
+        String str = null; // 条件1: nullを代入
+        System.out.println(str.length()); // 条件2: NullPointerExceptionが発生
+    }
+}
