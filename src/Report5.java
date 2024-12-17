6b01aafb6a2e1b537a03429a1ceab4eb79d4039d
Ryo SHIROMA <e245740@ie.u-ryukyu.ac.jp>
2024-12-17 19:00:20 +0900

    step2

--- src/Report5.java
+++ src/Report5.java
@@ -1,6 +1,12 @@
 public class Report5 {
     public static void main(String[] args) {
         String str = null; // 条件1: nullを代入
-        System.out.println(str.length()); // 条件2: NullPointerExceptionが発生
+
+        try {
+            System.out.println(str.length()); // 条件2: NullPointerExceptionが発生する
+        } catch (NullPointerException e) { // 条件2: 例外を捕捉
+            System.out.println("NullPointerExceptionが発生しました。"); // 条件2: 発生したことを出力
+            System.out.println("エラーメッセージ: " + e.getMessage()); // 条件3: getMessage()でメッセージ出力
+        }
     }
 }
